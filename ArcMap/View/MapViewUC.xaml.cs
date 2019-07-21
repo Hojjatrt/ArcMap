9b683a9900275461910204df117e0cbb8e1d573c
Hojjatrt <hojjatrt@gmail.com>
2019-07-21 10:29:08 +0430

    coordinate show and ruler tab to the mapview user control
    [x] show coordinate
    [x] angle from north
    [x] distance between 2 coordinates
    [x] polyline between 2 coords
    [ ] polyline with mousemove

--- ArcMap/View/MapViewUC.xaml.cs
+++ ArcMap/View/MapViewUC.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows;
 using System.Drawing;
 using Point = System.Windows.Point;
 using System.Windows.Input;
+using Esri.ArcGISRuntime.UI.Controls;
+using Color = System.Drawing.Color;
+using Esri.ArcGISRuntime.UI.GeoAnalysis;
 
 namespace ArcMap.View
 {
@@ -23,8 +26,13 @@ namespace ArcMap.View
         // Graphics overlay to host sketch graphics
         private Map myMap;
         private GraphicsOverlay _sketchOverlay;
+        private GraphicsOverlay _distanceOverlay;
         // Colors.
         private Color[] colors;
+        private bool start_distance;
+        private MapPoint first, last;
+        private double _distance,_angle;
+
         public MapViewUC()
         {
             colors = new Color[]
@@ -41,6 +49,7 @@ namespace ArcMap.View
             myMap = new Map();
             InitializeComponent();
             Initialize();
+            InitializeDistance();
             InitializeSketch();
         }
 
@@ -50,7 +59,8 @@ namespace ArcMap.View
 
             // Create the uri for the tiled layer
             Uri tiledLayerUri = new Uri(
-                "https://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer");
+            //"https://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer");
+            "http://localhost:6060/geoserver/test/wms");
 
             // Create a tiled layer using url
             ArcGISTiledLayer tiledLayer = new ArcGISTiledLayer(tiledLayerUri)
@@ -59,16 +69,16 @@ namespace ArcMap.View
             };
 
             //Add the tiled layer to map
-            myMap.OperationalLayers.Add(tiledLayer);
+            //myMap.OperationalLayers.Add(tiledLayer);
 
-            Uri serviceUri = new Uri(
-               "https://sampleserver6.arcgisonline.com/arcgis/rest/services/SampleWorldCities/MapServer");
+            Uri serviceUri = new Uri("http://localhost:6060/geoserver/test/wms");
+            //"https://sampleserver6.arcgisonline.com/arcgis/rest/services/SampleWorldCities/MapServer");
 
             // Create new image layer from the url
             ArcGISMapImageLayer imageLayer1 = new ArcGISMapImageLayer(serviceUri)
             {
                 Name = "World Cities Population",
-                Opacity = 0.5
+                //Opacity = 0.5
             };
 
             // Add created layer to the basemaps collection
@@ -89,7 +99,7 @@ namespace ArcMap.View
             };
 
             // Add the image layer to map
-            myMap.OperationalLayers.Add(imageLayer);
+            //myMap.OperationalLayers.Add(imageLayer);
 
             // Create Uri for feature layer
             Uri featureLayerUri = new Uri(
@@ -102,14 +112,14 @@ namespace ArcMap.View
             };
 
             // Add the feature layer to map
-            myMap.OperationalLayers.Add(myFeatureLayer);
+            //myMap.OperationalLayers.Add(myFeatureLayer);
 
             // Create a map point the map should zoom to
             MapPoint mapPoint = new MapPoint(-11000000, 4500000, SpatialReferences.WebMercator);
 
             // Set the initial viewpoint for map
             myMap.InitialViewpoint = new Viewpoint(mapPoint, 50000000);
-
+            myMap.Basemap = Basemap.CreateNationalGeographic();
             // Event for layer view state changed
             //MyMapView.LayerViewStateChanged += OnLayerViewStateChanged;
 
@@ -118,24 +128,26 @@ namespace ArcMap.View
             LayersListView.ItemsSource = myMap.OperationalLayers;
         }
 
+        private void InitializeDistance()
+        {
+            // Add a graphics overlay for showing the tapped point.
+            _distanceOverlay = new GraphicsOverlay();
+            start_distance = false;
+            SimpleMarkerSymbol markerSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.Black, 5);
+            _distanceOverlay.Renderer = new SimpleRenderer(markerSymbol);
+            MyMapView.GraphicsOverlays.Add(_distanceOverlay);
+            // Respond to user taps.
+            MyMapView.GeoViewTapped += MapView_Tapped;
+        }
         private void InitializeSketch()
         {
             // Create a light gray canvas map
             //Map myMap = new Map(Basemap.CreateLightGrayCanvas());
 
             // Create graphics overlay to display sketch geometry
-            _sketchOverlay = new GraphicsOverlay() {
+            _sketchOverlay = new GraphicsOverlay()
+            {
                 Id = "layer 1"
-            };
-            MyMapView.GraphicsOverlays.Add(_sketchOverlay);
-            _sketchOverlay = new GraphicsOverlay()
-            {
-                Id = "layer 2"
-            };
-            MyMapView.GraphicsOverlays.Add(_sketchOverlay);
-            _sketchOverlay = new GraphicsOverlay()
-            {
-                Id = "layer 3"
             };
             MyMapView.GraphicsOverlays.Add(_sketchOverlay);
             DataGrid_Graphiclayers.ItemsSource = MyMapView.GraphicsOverlays;
@@ -234,6 +246,70 @@ namespace ArcMap.View
         }
         #endregion
 
+        private void MapView_Tapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Get the tapped point - this is in the map's spatial reference,
+            // which in this case is WebMercator because that is the SR used by the included basemaps.
+            //MapPoint tappedPoint = e.Location;
+            MapPoint tappedPoint = MyMapView.ScreenToLocation(e.Position);
+            // Update the graphics.
+            //if (last != null && start_distance)
+            //{
+            //    end_distance_btn.Command.Execute(end_distance_btn.CommandParameter);
+            //    start_distance = false;
+            //}
+            if (start_distance == true)
+            {
+                _distanceOverlay.Graphics.Clear();
+                //// Project the point to WGS84
+                MapPoint projectedPoint = (MapPoint)GeometryEngine.Project(tappedPoint, SpatialReferences.Wgs84);
+                if (first == null)
+                    first = projectedPoint;
+                else
+                {
+                    last = projectedPoint;
+                    _distanceOverlay.Graphics.Add(new Graphic(first));
+                    
+                }
+                _distanceOverlay.Graphics.Add(new Graphic(tappedPoint));
+                
+
+
+                // Format the results in strings.
+                //string originalCoords = string.Format("Original: {0:F4}, {1:F4}", tappedPoint.X, tappedPoint.Y);
+                //MessageBox.Show(tappedPoint.GeometryType.ToString());
+                string projectedCoords = string.Format("Projected: {0:F4}, {1:F4}", projectedPoint.X, projectedPoint.Y);
+                string formattedString = string.Format("{0}", projectedCoords);
+
+                //Define a callout and show it in the map view.
+
+                if (projectedPoint == first)
+                {
+                    CalloutDefinition calloutDef = new CalloutDefinition("Coordinates:", formattedString);
+                    MyMapView.ShowCalloutAt(tappedPoint, calloutDef);
+                }
+                else
+                {
+                    coordinate_text.Text = formattedString;
+                    _distance = distance(first.Y, first.X, last.Y, last.X, 'N');
+                    distance_text.Text = _distance.ToString();
+                    _angle = DegreeBearing(first.Y, first.X, last.Y, last.X);
+                    angle_text.Text = _angle.ToString();
+                    MapPoint[] arr = new MapPoint[2];
+                    arr[0] = first;
+                    arr[1] = last;
+                    Polyline pline = new Polyline(arr);
+                    Color creationColor = Color.Red;
+                    // Create and add a graphic from the geometry the user drew
+                    Graphic graphic = CreateGraphic(pline, creationColor);
+                    _distanceOverlay.Graphics.Add(graphic);
+                    start_distance = false;
+
+                }
+
+            }
+        }
+
         private async void DrawButtonClick(object sender, RoutedEventArgs e)
         {
             try
@@ -297,12 +373,14 @@ namespace ArcMap.View
             }
         }
 
+        #region panel_btns
         private void draw_sketch_btn_Click(object sender, RoutedEventArgs e)
         {
             if (sketch_panel.Visibility == Visibility.Collapsed)
             {
-                layers_panel.Visibility = Visibility.Collapsed;
                 sketch_panel.Visibility = Visibility.Visible;
+                layers_panel.Visibility = Visibility.Collapsed;
+                distance_panel.Visibility = Visibility.Collapsed;
             }
             else
                 sketch_panel.Visibility = Visibility.Collapsed;
@@ -314,11 +392,24 @@ namespace ArcMap.View
             {
                 sketch_panel.Visibility = Visibility.Collapsed;
                 layers_panel.Visibility = Visibility.Visible;
+                distance_panel.Visibility = Visibility.Collapsed;
             }
             else
                 layers_panel.Visibility = Visibility.Collapsed;
         }
 
+        private void distance_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (distance_panel.Visibility == Visibility.Collapsed)
+            {
+                sketch_panel.Visibility = Visibility.Collapsed;
+                layers_panel.Visibility = Visibility.Collapsed;
+                distance_panel.Visibility = Visibility.Visible;
+            }
+            else
+                distance_panel.Visibility = Visibility.Collapsed;
+        }
+        #endregion
         private void plusbtn_Click(object sender, RoutedEventArgs e)
         {
             GraphicsOverlay g = ((Button)sender).DataContext as GraphicsOverlay;
@@ -354,5 +445,95 @@ namespace ArcMap.View
         {
             DataGrid_Graphiclayers.UnselectAll();
         }
+
+        private void start_distance_btn_Click(object sender, RoutedEventArgs e)
+        {
+            _distanceOverlay.Graphics.Clear();
+            MyMapView.DismissCallout();
+            start_distance = true;
+            first = last = null;
+        }
+
+        private void MyMapView_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (start_distance && first != null)
+            {
+                MapPoint Point = MyMapView.ScreenToLocation(e.GetPosition(MyMapView));
+                _distanceOverlay.Graphics.Clear();
+                //// Project the point to WGS84
+                MapPoint projectedPoint = (MapPoint)GeometryEngine.Project(Point, SpatialReferences.Wgs84);
+                string projectedCoords = string.Format("Projected: {0:F4}, {1:F4}", projectedPoint.X, projectedPoint.Y);
+                string formattedString = string.Format("{0}", projectedCoords);
+                coordinate_text.Text = formattedString;
+                _distance = distance(first.Y, first.X, projectedPoint.Y, projectedPoint.X, 'N');
+                distance_text.Text = _distance.ToString();
+                _angle = DegreeBearing(first.Y, first.X, projectedPoint.Y, projectedPoint.X);
+                angle_text.Text = _angle.ToString();
+                MapPoint[] arr = new MapPoint[2];
+                arr[0] = first;
+                arr[1] = projectedPoint;
+                Polyline pline = new Polyline(arr);
+                Color creationColor = Color.Red;
+                // Create and add a graphic from the geometry the user drew
+                Graphic graphic = CreateGraphic(pline, creationColor);
+                _distanceOverlay.Graphics.Add(graphic);
+            }
+        }
+
+        #region angle meaturment
+        static double DegreeBearing(double lat1, double lon1, double lat2, double lon2)
+        {
+            var dLon = ToRad(lon2 - lon1);
+            var dPhi = Math.Log(
+                Math.Tan(ToRad(lat2) / 2 + Math.PI / 4) / Math.Tan(ToRad(lat1) / 2 + Math.PI / 4));
+            if (Math.Abs(dLon) > Math.PI)
+                dLon = dLon > 0 ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
+            return ToBearing(Math.Atan2(dLon, dPhi));
+        }
+
+        public static double ToRad(double degrees)
+        {
+            return degrees * (Math.PI / 180);
+        }
+
+        public static double ToDegrees(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+
+        public static double ToBearing(double radians)
+        {
+            // convert radians to degrees (as bearing: 0...360)
+            return (ToDegrees(radians) + 360) % 360;
+        }
+        #endregion
+
+        #region distance meaturment
+        private double distance(double lat1, double lon1, double lat2, double lon2, char unit)
+        {
+            if ((lat1 == lat2) && (lon1 == lon2))
+            {
+                return 0;
+            }
+            else
+            {
+                double theta = lon1 - lon2;
+                double dist = Math.Sin(ToRad(lat1)) * Math.Sin(ToRad(lat2)) + Math.Cos(ToRad(lat1)) * Math.Cos(ToRad(lat2)) * Math.Cos(ToRad(theta));
+                dist = Math.Acos(dist);
+                dist = ToDegrees(dist);
+                dist = dist * 60 * 1.1515;
+                if (unit == 'K')
+                {
+                    dist = dist * 1.609344;
+                }
+                else if (unit == 'N')
+                {
+                    dist = dist * 0.8684;
+                }
+                return (dist);
+            }
+        }
+        #endregion
     }
 }
